2b11d8f7913de78eee03139c7d3c6b685e6456d3
Yomna Magdy Soliman <36536078+YomnaMagdy@users.noreply.github.com>
2018-02-19 18:32:34 +0200

    Add files via upload
    examples for Arduino

---
+++ sheet1_problem_1.ino
@@ -0,0 +1,13 @@
+int pin1=3;
+void setup() {
+
+pinMode(pin1,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(pin1,HIGH);
+  delay(0.0005);
+  digitalWrite(pin1,LOW);
+  delay(0.0015);
+
+}
---
+++ sheet_1_problem_2.ino
@@ -0,0 +1,45 @@
+int a=4;
+int b=5;
+int c=6;
+int d=7;
+void setup() {
+pinMode(a,OUTPUT);
+pinMode(b,OUTPUT);
+pinMode(c,OUTPUT);
+pinMode(d,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(a,HIGH);
+  delay(1000);
+  digitalWrite(a,LOW);
+  delay(1000);
+  digitalWrite(b,HIGH);
+  delay(1000);
+  digitalWrite(b,LOW);
+  delay(1000);
+  digitalWrite(c,HIGH);
+  delay(1000);
+  digitalWrite(c,LOW);
+  delay(1000);
+  digitalWrite(d,HIGH);
+  delay(1000);
+  digitalWrite(d,LOW);
+  delay(1000);
+  digitalWrite(d,HIGH);
+  delay(1000);
+  digitalWrite(d,LOW);
+  delay(1000);
+  digitalWrite(c,HIGH);
+  delay(1000);
+  digitalWrite(c,LOW);
+  delay(1000);
+  digitalWrite(b,HIGH);
+  delay(1000);
+  digitalWrite(b,LOW);
+  delay(1000);
+  digitalWrite(a,HIGH);
+  delay(1000);
+  digitalWrite(a,LOW);
+  delay(1000);
+}
