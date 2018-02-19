82e76917aac695661a77598fdaecb50d8c0f420b
omar mohamd <36541876+omarmohamed77@users.noreply.github.com>
2018-02-19 18:52:01 +0200

    Add files via upload

---
+++ sheet1q1.ino
@@ -0,0 +1,15 @@
+// question 1
+char led=13;
+unsigned int dutyCycle=2000;
+void setup() {
+ pinMode (led,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite (led,HIGH);
+  delay (dutyCycle*.25);
+  digitalWrite (led,LOW);
+  delay (dutyCycle*.75);
+
+}
