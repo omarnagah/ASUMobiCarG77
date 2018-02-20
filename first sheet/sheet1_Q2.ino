265a227a4945dc2f1f029007fcc86fbe837f3162
AmrHosam <amrhossam97@gmail.com>
2018-02-20 19:17:24 +0200

    Q2

--- first sheet/sheet1_Q2.ino
+++ first sheet/sheet1_Q2.ino
@@ -1,34 +1,24 @@
 int waitTime = 1000;
-int LED1 = 4;
-int LED2 = 5;
-int LED3 = 6;
-int LED4 = 7;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED1 , OUTPUT);
-  pinMode(LED2 , OUTPUT);
-  pinMode(LED3 , OUTPUT);
-  pinMode(LED4 , OUTPUT);
+  pinMode(4 , OUTPUT);
+  pinMode(5 , OUTPUT);
+  pinMode(6 , OUTPUT);
+  pinMode(7 , OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(LED1 , HIGH);
-delay(waitTime);
-digitalWrite(LED1 , LOW);
-digitalWrite(LED2 , HIGH);
-delay(waitTime);
-digitalWrite(LED2 , LOW);
-digitalWrite(LED3 , HIGH);
-delay(waitTime);
-digitalWrite(LED3 , LOW);
-digitalWrite(LED4 , HIGH);
-delay(waitTime);
-digitalWrite(LED3 , HIGH);
-digitalWrite(LED4 , LOW);
-delay(waitTime);
-digitalWrite(LED2 , HIGH);
-digitalWrite(LED3 , LOW);
-delay(waitTime);
-digitalWrite(LED2 , LOW);
+  for(int i = 4 ; i<=7 ; i++)
+  {
+    digitalWrite(i , HIGH);
+    delay(waitTime);
+    digitalWrite(i , LOW);
+  }
+    for(int i = 6 ; i>=4 ; i--)
+  {
+    digitalWrite(i , HIGH);
+    delay(waitTime);
+    digitalWrite(i , LOW);
+  }
 }
