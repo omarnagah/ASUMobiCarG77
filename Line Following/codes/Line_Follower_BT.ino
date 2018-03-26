6d902153f99255ae2654f1376c4fd2453f3d4e5e
Omar Shafik <1500927@eng.asu.edu.eg>
2018-03-26 17:56:21 +0200

    Add files via upload

---
+++ Line Following/codes/Line_Follower_BT.ino
@@ -0,0 +1,34 @@
+#define BTread Serial.read()
+#define CalSBY 'W'
+#define RaceSBY 'U'
+#define CalKey 'w'
+#define RaceKey 'u'
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(300);
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    if (BTread == CalSBY) {
+      while (BTread == CalSBY) {
+        //calibration stand-by
+      }
+      if (BTread == CalKey) {
+        //calibrate
+      }
+    }
+    else if (BTread == RaceSBY) {
+      while (BTread == RaceSBY) {
+        //race stand-by
+      }
+      if (BTread == RaceKey) {
+        while ((BTread != RaceSBY) && (BTread != CalSBY)) {   //press cal- or race-standby to stop
+          //race
+        }
+      }
+    }
+  }
+}
