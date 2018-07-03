afa4ee1b9b3f99614bc7abd0b4984ef82069450c
Omar Shafik <1500927@eng.asu.edu.eg>
2018-07-03 23:00:34 +0200

    Add files via upload

---
+++ Accurate Movement/phase_3_Bluetooth.ino
@@ -0,0 +1,41 @@
+#define Sread Serial.read()
+#define sby 'S'
+#define zeroASCII 48
+
+unsigned char slider = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char data = Sread;
+    if (data == 'O') {
+      while (data != 'o') {
+        if (data >= zeroASCII && data <= zeroASCII + 9) {
+          slider = data - zeroASCII;
+        }
+        else {
+          unsigned char distance = slider * 10 + 10;
+          if ( data == 'F') {
+
+          }
+          if ( data == 'R') {
+
+          }
+          if ( data == 'L') {
+
+          }
+          if ( data == 'B') {
+
+          }
+        }
+        data = Sread;
+      }
+    }
+    else if (data == 'P') {
+      //do demo
+    }
+  }
+}
